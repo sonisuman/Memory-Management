f732118b2f199a5b3ce019d5b5e6aa5e8aeb28e3
soni suman <sonisuman@Sonis-MacBook-Pro.local>
2020-01-10 12:45:51 +0530

    Bit operations and Bit shifting left and right & and or operations Implementation.

--- Integers/Integers.playground/Contents.swift
+++ Integers/Integers.playground/Contents.swift
@@ -29,8 +29,50 @@ Int(a) + 100
 0x0A
 
 UInt8(128).binaryDescription
-Int8(-128).binaryDescription
 Int8(127).binaryDescription
+Int8(-128).binaryDescription
+
+
+UInt8(255).binaryDescription
+let b = UInt8(255)
+let c = Int8(bitPattern: b)
+UInt8(bitPattern: c)
+
+UInt8(250.byteDescriptions)
+
+UInt16(bigEndian: 0xff)
+UInt16(littleEndian: 0xff)
+UInt16(0xff)
+
+
+var dataVal = Int32(0x0A_0B_0C_0D).bigEndian
+let bufferPointer = UnsafeRawBufferPointer(start: &dataVal, count: MemoryLayout.size(ofValue: dataVal))
+
+for element in bufferPointer {
+    print(String(element,radix: 16))
+}
+
+ //&+ :- used to maintain over flow and &- :- used to maintain under flow for small value
+
+let largeInt = 0xff03
+UInt8(truncatingIfNeeded: largeInt)
+
+UInt8(255) &+ 1
+UInt8(0) &- 1
+
+
+let bits = 0b00110
+bits >> 2
+bits << 2
+bits | 0b001
+bits & 0b00100
+
+
+UInt8(exactly: 255)
+UInt8(exactly: 256)
+
+
+Int8(16).multipliedReportingOverflow(by: 8)
 
 /*
  Note: -
